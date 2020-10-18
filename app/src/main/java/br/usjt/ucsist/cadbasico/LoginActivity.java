556eafd8e58e8315dd96d2c8961b467c2bba822e
Michael Nogueira <michaelnogueira.8237@aluno.saojudas.br>
2020-10-17 21:22:13 -0300

    restante da implementação da aula de sexta

--- app/src/main/java/br/usjt/ucsist/cadbasico/LoginActivity.java
+++ app/src/main/java/br/usjt/ucsist/cadbasico/LoginActivity.java
@@ -1,20 +1,62 @@
 package br.usjt.ucsist.cadbasico;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import com.orhanobut.hawk.Hawk;
 
 public class LoginActivity extends AppCompatActivity {
+    private TextView textViewNovoCadastro;
+    private Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Hawk.init(this).build();
+
+        textViewNovoCadastro = findViewById(R.id.textViewNovoCadastro);
+        buttonLogin = findViewById(R.id.buttonLogin);
+
+
     }
 
-    //aqui é onde faço a transição das Telas
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (Hawk.contains("tem_cadastro")){
+            if(Hawk.get("tem_cadastro")){
+                habilitarLogin();
+            }else{
+                desabilitarLogin();
+            }
+        }else{
+            desabilitarLogin();
+        }
+    }
+
+
+    //abaixo é onde habilito e desabilito o login
+
+    private void habilitarLogin(){
+        textViewNovoCadastro.setVisibility(View.GONE);
+        buttonLogin.setEnabled(true);
+        buttonLogin.setBackgroundColor(getColor(R.color.colorPrimary));
+    }
+
+    private void desabilitarLogin(){
+        textViewNovoCadastro.setVisibility(View.VISIBLE);
+        buttonLogin.setEnabled(false);
+        buttonLogin.setBackgroundColor(R.color.gray);
+    }
+
+    //abaixo é onde faço a transição das Telas
+
     public void novoCadastro(View view) {
         Intent intent = new Intent(this, CadastroActivity.class);
         startActivity (intent);
